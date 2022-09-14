5444a92cebb2d4636f775552b79d402c93a9bebf
Oguma1028 <neetoguma@gmail.com>
2022-09-14 12:44:32 +0900

    型を設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import styles from "../styles/Home.module.scss";
 import MainLayout from "../layouts";
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.topArticles);
   return (
     <MainLayout>
       <Head>
@@ -10,3 +12,19 @@ export default function Home() {
     </MainLayout>
   );
 }
+
+export const getStaticProps = async () => {
+  const pageSize = 10;
+  const topRes = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=jp&category=technology&pageSize=${pageSize}&apiKey=3f820aff78014c0aa3713493a5f93480`
+  );
+  const topJson = await topRes.json();
+  const topArticles = topJson?.article;
+
+  return {
+    props: {
+      topArticles,
+    },
+    revalidate: 60 * 10,
+  };
+};
